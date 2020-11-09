f1a9f6dc1fa0d28bce42d11794db363d8bb688ef
dshulgaster <dshulgaster@gmail.com>
2020-11-09 19:59:58 +0300

    les08, Sapper

--- src/main/java/gbjc0/les08/App.java
+++ src/main/java/gbjc0/les08/App.java
@@ -32,28 +32,27 @@ import java.util.Arrays;
                 button.addActionListener(actionEvent -> {
                     if (!isAlive) return;
                     if (bombs[ii][jj]) {
+                        button.setText("\u2620");
+                        button.setFont(button.getFont().deriveFont(42.F));
+                        button.setBackground(Color.RED);
+                        button.setForeground(Color.WHITE);
+
                         //ОТКРЫТЬ ВСЕ ПОЛЯ С БОМБАМИ
-                        System.out.println("ii = " + ii + ", jj = " + jj);
                         for (int k = 0; k < SIZE; k++) {
                             for (int l = 0; l < SIZE; l++) {
                                 if (!bombs[k][l]) {
                                     buttons[k][l].setText("VVV");
                                 } else {
                                     if ((k==ii) && (l==jj)) {
-                                        System.out.println("Текущая кнопка");
+                                        continue;
+//                                        System.out.println("Текущая кнопка");
                                     }
-                                    System.out.println("else ");
                                     buttons[k][l].setText("\u2620");
                                     buttons[k][l].setFont(button.getFont().deriveFont(38.F));
                                     buttons[k][l].setBackground(Color.DARK_GRAY);
                                     buttons[k][l].setForeground(Color.WHITE);
                                 }
                             }
-                            // установку красной бомбы ставим в конец, чтобы не затерлась другим цветом
-                            button.setText("\u2620");
-                            button.setFont(button.getFont().deriveFont(42.F));
-                            button.setBackground(Color.RED);
-                            button.setForeground(Color.WHITE);
                         }
                         isAlive = false;
                     } else {
@@ -74,57 +73,43 @@ import java.util.Arrays;
 
         // проверка нижнего поля
         if (((ii+1) < bombs.length) && (bombs[ii+1][jj])) {
-            System.out.println("ii+1 = " + (ii + 1) + ", j = " + jj + ", бомба снизу = " + bombs[ii+1][jj] + ", bombs.length = " + bombs.length);
             countBombs++;
-        } else System.out.println("ii+1 = " + (ii + 1) + ", j = " + jj + ", бомбы снизу нет (или поле снизу закончилось)");
+        }
 
         // проверка верхнего поля
         if (((ii-1) >= 0) && (bombs[ii-1][jj])) {
-            System.out.println("ii-1 = " + (ii - 1) + ", j = " + jj + ", бомба сверху = " + bombs[ii-1][jj] + ", bombs.length = " + bombs.length);
             countBombs++;
-        } else System.out.println("ii-1 = " + (ii - 1) + ", j = " + jj + ", бомбы сверху нет (или поле сверху закончено)");
+        }
 
         // проверка правого поля
         if (((jj+1) < bombs.length) && (bombs[ii][jj+1])) {
-            System.out.println("ii = " + ii + ", jj+1 = " + (jj+1) + ", бомба справа = " + bombs[ii][jj+1] + ", bombs.length = " + bombs.length);
             countBombs++;
-        } else System.out.println("ii = " + ii + ", jj+1 = " + (jj+1) + ", бомбы справа нет (или поле справа закончилось)");
+        }
 
         // проверка левого поля
         if (((jj-1) >= 0) && (bombs[ii][jj-1])) {
-            System.out.println("ii = " + ii + ", jj-1 = " + (jj-1) +
-                    ", бомба слева = " + bombs[ii][jj-1] + ", bombs.length = " + bombs.length);
             countBombs++;
-        } else System.out.println("ii = " + ii + ", jj-1 = " + (jj-1) + ", бомбы слева нет (или поле справа закончилось)");
+        }
 
         // проверка нижней левой диагонали
         if (((ii+1) < bombs.length) && ((jj-1) >= 0) && (bombs[ii+1][jj-1])) {
-            System.out.println("ii+1 = " + (ii + 1) + ", jj-1 = " + (jj-1) +
-                    ", бомба в нижней левой диагонали = " + bombs[ii+1][jj-1] + ", bombs.length = " + bombs.length);
             countBombs++;
-        } else System.out.println("ii+1 = " + (ii + 1) + ", jj-1 = " + (jj-1) + ", бомбы в нижней левой диагонали нет(или поле снизу закончилось)");
+        }
 
         // проверка верхней левой диагонали
         if (((ii-1) >= 0) && ((jj-1) >= 0) && (bombs[ii-1][jj-1])) {
-            System.out.println("ii-1 = " + (ii - 1) + ", jj-1 = " + (jj-1) +
-                    ", бомба в верхней левой диагонали = " + bombs[ii-1][jj-1] + ", bombs.length = " + bombs.length);
             countBombs++;
-        } else System.out.println("ii-1 = " + (ii - 1) + ", jj-1 = " + (jj-1) + ", бомбы в верхней левой диагонали нет(или поле снизу закончилось)");
+        }
 
         // проверка нижней правой диагонали
         if (((ii+1) < bombs.length) && ((jj+1) < bombs.length) && (bombs[ii+1][jj+1])) {
-            System.out.println("ii+1 = " + (ii + 1) + ", jj+1 = " + (jj+1) +
-                    ", бомба в нижней правой диагонали = " + bombs[ii+1][jj+1] + ", bombs.length = " + bombs.length);
             countBombs++;
-        } else System.out.println("ii+1 = " + (ii + 1) + ", jj+1 = " + (jj+1) + ", бомбы в нижней правой диагонали нет(или поле снизу закончилось)");
+        }
 
         // проверка верхней правой диагонали
         if (((ii-1) >= 0) && ((jj+1) < bombs.length) && (bombs[ii-1][jj+1])) {
-            System.out.println("ii-1 = " + (ii - 1) + ", jj+1 = " + (jj+1) +
-                        ", бомба в верхней правой диагонали = " + bombs[ii-1][jj+1] + ", bombs.length = " + bombs.length);
             countBombs++;
-        } else System.out.println("ii-1 = " + (ii - 1) + ", jj+1 = " + (jj+1) + ", бомбы в верхней правой диагонали нет(или поле снизу закончилось)");
-
+        }
         return String.valueOf(countBombs);
     }
 }
